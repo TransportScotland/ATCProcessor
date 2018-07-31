4ebd1669f8fde7dcca336be456917fa3d76b5419
adam <adam.ruszkowski@gmail.com>
2018-07-31 15:56:53 +0100

    Added initial setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from atcprocessor.version import __version__
+
+setup(
+    name='atcprocessor',
+    version=__version__,
+    packages=['atcprocessor'],
+    url='',
+    license='',
+    author='Transport Scotland',
+    author_email='',
+    description=''
+)
