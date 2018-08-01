3dc7fb6daecff754bc7a0cc9978ffd38abc51e59
adam <adam.ruszkowski@gmail.com>
2018-08-01 10:03:00 +0100

    Outline menu bar, plus reading and loading of settings :tada:

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import os
 from collections import defaultdict
 from glob import glob
+import json
 try:
     import tkinter as tk
     from tkinter import filedialog, ttk, messagebox
@@ -19,18 +20,69 @@ class ATCProcessorGUI(tk.Frame):
         super(ATCProcessorGUI, self).__init__(parent, padx=5, pady=5,
                                               *args, **kwargs)
 
-        inps = FileInputs(
+        self.store = dict()
+
+        self.store['File Inputs'] = FileInputs(
             self, section_name='File Inputs',
-            input_names=('Input Folder', 'Site List', 'Thresholds')
+            input_names=('Input Folder', 'Site List File', 'Thresholds File',
+                         'Output Folder')
         )
-        inps.grid(row=1, column=0, sticky='WE')
+        self.store['File Inputs'].grid(row=1, column=0, sticky='WE')
 
-        cols = ColumnSelector(self, section_name='Choose Columns',
-                              input_names=('Option 1', 'Option 2'),
-                              folder_variable=inps.variables['Input Folder'])
+        columns_to_choose = (
+            'Site Name Column', 'Count Column', 'Direction Column',
+            'Date Column', 'Time Column'
+        )
 
-        cols.grid(row=2, column=0, sticky='WE')
+        self.store['Columns'] = ColumnSelector(
+            self, section_name='Choose Columns', input_names=columns_to_choose,
+            folder_variable=self.store['File Inputs'].variables['Input Folder']
+        )
+
+        self.store['Columns'].grid(row=2, column=0, sticky='WE')
+
         self.grid_columnconfigure(0, weight=1)
+
+        # Set up menu bar for advanced settings
+        menu_bar = tk.Menu(parent)
+        parent.config(menu=menu_bar)
+        options = tk.Menu(menu_bar, tearoff=False)
+
+        menu_bar.add_cascade(label='Options', menu=options)
+
+        # TODO set up advanced settings
+        # options.add_command(label='Advanced Settings')
+        # options.add_separator()
+        options.add_command(label='Load Settings',
+                            command=lambda: self.load_settings())
+        options.add_command(label='Save Settings',
+                            command=lambda: self.save_settings())
+
+    def save_settings(self):
+        all_settings = {
+            name: {k: v.get() for k, v in widget.variables.items()}
+            for name, widget in self.store.items()
+        }
+
+        res = filedialog.asksaveasfilename(
+            defaultextension='.json',
+            filetypes=[('JSON file', '*.json')]
+        )
+        if res:
+            with open(res, 'w') as f:
+                json.dump(all_settings, f, indent=4)
+
+    def load_settings(self):
+        res = filedialog.askopenfilename(
+            filetypes=[('JSON file', '*.json')]
+        )
+        if res:
+            with open(res, 'r') as f:
+                all_settings = json.load(f)
+
+            for name, widget in self.store.items():
+                for k, v in all_settings[name].items():
+                    widget.variables[k].set(v)
 
 
 class FileInputs(tk.LabelFrame):
@@ -79,7 +131,7 @@ class ColumnSelector(tk.LabelFrame):
 
         self.variables = {inp: tk.StringVar() for inp in input_names}
         self.folder_variable = folder_variable
-        self.values = []
+        self.values = ['<Columns not loaded>']
 
         butt = tk.Button(self, text='Update Column Choices',
                          command=lambda: self.update_choices())
@@ -90,19 +142,26 @@ class ColumnSelector(tk.LabelFrame):
             choice = ttk.Combobox(self, values=self.values,
                                   textvariable=var)
 
-            choice.configure(postcommand=lambda choice=choice: self.combo_update(box=choice))
+            choice.configure(
+                postcommand=lambda choice=choice: self.combo_update(box=choice)
+            )
 
-            lab.grid(row=i+1, column=0)
-            choice.grid(row=i+1, column=1)
+            lab.grid(row=i+1, column=0, sticky='W')
+            choice.grid(row=i+1, column=1, sticky='WE')
 
     def combo_update(self, box):
         box['values'] = self.values
 
     def update_choices(self):
-        # TODO make this read from a file. The first in
+        # TODO validate that a file exists.
         first_file = glob(os.path.join(self.folder_variable.get(), '*.csv'))[0]
         with open(first_file, 'r') as f:
             columns = f.readline().split(',')
+
+        # Line will probably end with a carriage return
+        # TODO consider a single-line file (i.e. no data). What happens?
+        if columns[-1].endswith('\n'):
+            columns[-1] = columns[-1][:-1]
         self.values = columns
 
 
