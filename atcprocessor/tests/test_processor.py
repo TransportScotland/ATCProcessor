52a6c48e9de96939ba134741a64f7334502c5e5e
Adam Ruszkowski <adam.ruszkowski@gmail.com>
2018-08-02 17:14:03 +0100

    Trying to fix using data files in Travis tests

--- atcprocessor/tests/test_processor.py
+++ atcprocessor/tests/test_processor.py
@@ -11,9 +11,8 @@ def datadir(tmpdir, request):
     """
     Copies the directory containing test files to a location for further use.
     """
-    filename = request.module.__file__
-    test_dir, _ = os.path.split(filename)
-    dir_util.copy_tree(os.path.join(test_dir, 'test files'), str(tmpdir))
+    dir_util.copy_tree(os.path.join(os.path.dirname(__file__), 'test files'),
+                       str(tmpdir))
 
     return tmpdir
 
@@ -22,8 +21,6 @@ class TestProcessor:
     @pytest.fixture(autouse=True)
     def setup(self, tmpdir_factory, datadir):
         self.output_folder = tmpdir_factory.mktemp('Outputs')
-
-        self.datadir = datadir
 
         self.thresholds = processor.Thresholds(
             path_to_csv=os.path.join(datadir, 'thresholds.csv'),
@@ -42,15 +39,14 @@ class TestProcessor:
             thresholds=self.thresholds
         )
 
-    def test_cleaning(self):
+    def test_cleaning(self, datadir):
         self.count_site.clean_data()
-
         cleaning_result = pd.read_csv(
             os.path.join(self.output_folder, 'cleaned data', 'site 1.csv')
         )
 
         known_clean = pd.read_csv(
-            os.path.join(self.datadir, 'outputs', 'cleaned data',
+            os.path.join(datadir, 'outputs', 'cleaned data',
                          'site 1.csv')
         )
 
