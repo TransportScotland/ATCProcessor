f495cdbaba52b94cd8d8b7b16f00b9465687a7cf
Adam Ruszkowski <adam.ruszkowski@gmail.com>
2018-08-07 16:43:40 +0100

    Tweaks to use of setup.py to hopefully fix Python 2.7 testing

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from atcprocessor.version import __version__
 setup(
     name='atcprocessor',
     version=__version__,
-    packages=['atcprocessor'],
+    packages=['atcprocessor', 'atcprocessor.calmap'],
     url='',
     license='',
     author='Transport Scotland',
